5732eca79438c3d170f8078fcbd82d6570cd8990
Seregy <seregy77@gmail.com>
2021-10-15 20:52:06 +0300

    Add package.json with web-ext
    - add package.json along with script for running firefox from web-ext
    - rename `content_script.js` to `content-script.js`

---
+++ web-ext-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: false,
+  build: {overwriteDest: true},
+  run: {
+    startUrl: ["about:debugging#/runtime/this-firefox"],
+    firefox: "firefoxdeveloperedition",
+  },
+};
