f04badc68465f960942ff14e055ce9e85d536337
Seregy <seregy77@gmail.com>
2021-10-16 22:23:58 +0300

    Refactor expand-collapse in main script
    - move script initialization into separate function
    - move expand-collapse button initialization out of footer
    initialization

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,15 +1,39 @@
 import { ExpandCollapseElement } from "./element/expand-collapse";
 
-const shortFooterSettings = browser.storage.sync.get("shortFooter");
-shortFooterSettings.then(handleFooterDisplay, onError);
-initTableObserver();
-const expandCollapseElement = new ExpandCollapseElement(handleSwitchToExpandableState, handleSwitchToCollapsableState);
+init();
 
-function initTableObserver() {
+function init() {
+  const shortFooterSettings = browser.storage.sync.get("shortFooter");
+  shortFooterSettings.then(handleFooterDisplay, onError);
+
+  handleExpandCollapseDisplay({expandAll: true});
+}
+
+function handleFooterDisplay(footerSettings) {
+  if (footerSettings.shortFooter) {
+    const worklogFooter = document.querySelector("div.worklog__footer");
+
+    console.assert(!worklogFooter.hasAttribute("id"), "Worklog footer already has an ID assigned to it");
+    worklogFooter.id = "tes-worklog-footer";
+  }
+}
+
+function handleExpandCollapseDisplay(expandCollapseSettings) {
+  if (expandCollapseSettings.expandAll) {
+    const expandCollapseElement = new ExpandCollapseElement(handleSwitchToExpandableState, handleSwitchToCollapsableState);
+
+    const headerCell = document.querySelector("#head");
+    headerCell.prepend(expandCollapseElement.htmlElement);
+
+    initTableObserver(expandCollapseElement);
+  }
+}
+
+function initTableObserver(expandCollapseElement) {
   let observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
     });
-    initToggleStuff();
+    initToggleStuff(expandCollapseElement);
   });
 
   observer.observe(document.querySelector("mat-table"), {
@@ -17,21 +41,9 @@ function initTableObserver() {
   });
 }
 
-function handleFooterDisplay(footerSettings) {
-  if (footerSettings.shortFooter) {
-    const worklogFooter = document.querySelector("div.worklog__footer");
-
-    const headerCell = document.querySelector("#head");
-    headerCell.prepend(expandCollapseElement.htmlElement);
-
-    console.assert(!worklogFooter.hasAttribute("id"), "Worklog footer already has an ID assigned to it");
-    worklogFooter.id = "tes-worklog-footer";
-  }
-}
-
-function initToggleStuff() {
+function initToggleStuff(expandCollapseElement) {
   const allUnmanagedRows = document.querySelectorAll("mat-row.groupRow:not(.tes-row)");
-  
+
   for (const unmanagedRow of allUnmanagedRows) {
     const nextElement = unmanagedRow.nextElementSibling;
     if (nextElement && !nextElement.classList.contains("groupRow")) {
@@ -40,19 +52,19 @@ function initToggleStuff() {
       unmanagedRow.classList.add("tes-row", "tes-row-collapsed");
     }
 
-    unmanagedRow.addEventListener('click', toggleRow);
+    unmanagedRow.addEventListener('click', event => toggleRow(event, expandCollapseElement));
   }
 }
 
-function toggleRow(event) {
+function toggleRow(event, expandCollapseElement) {
   const target = event.target.closest(".tes-row-collapsed, .tes-row-expanded");
   target.classList.toggle("tes-row-collapsed");
   target.classList.toggle("tes-row-expanded");
 
-  toggleExpandCollapseElement();
+  toggleExpandCollapseElement(expandCollapseElement);
 }
 
-function toggleExpandCollapseElement() {
+function toggleExpandCollapseElement(expandCollapseElement) {
   const collapsedRows = document.querySelectorAll("mat-row.tes-row-collapsed");
 
   if (expandCollapseElement.isExpandable) {
