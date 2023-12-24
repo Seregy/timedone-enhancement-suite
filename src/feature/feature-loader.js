65af78e6f88285b0d7bd756d98ada89a729f7add
Serhii Shymkov <Seregy77@gmail.com>
2023-12-24 20:33:19 +0200

    Remove the project autoselect bugfix (#50)
    - remove the project autoselect bugfix feature
    
    Project autoselect seems to work fine without this feature being enabled, which means that it was probably fixed in the original app

--- src/feature/feature-loader.js
+++ src/feature/feature-loader.js
@@ -1,12 +1,10 @@
-import projectAutoselectBugfix from
-  './project-autoselect-bugfix/project-autoselect-bugfix.js';
 import projectsTime from './projects-time/projects-time.js';
 
 /**
  * @typedef {import('./../feature/feature.js').Feature} Feature
  */
 
-const features = [projectAutoselectBugfix, projectsTime];
+const features = [projectsTime];
 const featuresSorted = features.sort((first, second) =>
   first.getId().localeCompare(second.getId()));
 
